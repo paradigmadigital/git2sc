3b172cd94b357ae68496bf582bc7088e2349b842
jamatute <jamatute@paradigmadigital.com>
2018-07-10 14:12:01 +0200

    Added docstrings to the tests

--- tests/unit/test_cli.py
+++ tests/unit/test_cli.py
@@ -1,6 +1,4 @@
-import pytest
 import unittest
-from unittest.mock import MagicMock, patch, call
 from git2sc.cli import load_parser
 
 
@@ -9,6 +7,9 @@ class TestArgparse(unittest.TestCase):
         self.parser = load_parser()
 
     def test_has_subcommand_article(self):
+        '''Required to ensure that the parser is correctly configured to
+        update an article as expected'''
+
         parsed = self.parser.parse_args(['article', '1', 'index.html'])
         self.assertEqual(parsed.subcommand, 'article')
         self.assertEqual(parsed.article_id, '1')
