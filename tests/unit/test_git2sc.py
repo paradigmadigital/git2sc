747182bd9be62e76dc2e1c8181af1b0d26dfd9fb
jamatute <jamatute@paradigmadigital.com>
2018-07-17 09:17:58 +0200

    Expandido self.g a self.git2sc

--- tests/unit/test_git2sc.py
+++ tests/unit/test_git2sc.py
@@ -11,7 +11,7 @@ class TestGit2SC(unittest.TestCase):
         self.api_url = 'https://confluence.sucks.com/wiki/rest/api'
         self.auth_string = 'user:password'
         self.auth = tuple(self.auth_string.split(':'))
-        self.g = Git2SC(self.api_url, self.auth_string)
+        self.git2sc = Git2SC(self.api_url, self.auth_string)
 
         self.requests_patch = patch('git2sc.git2sc.requests')
         self.requests = self.requests_patch.start()
@@ -28,24 +28,24 @@ class TestGit2SC(unittest.TestCase):
     def test_has_auth_set(self):
         'Required attribute for some methods'
 
-        self.assertEqual(self.g.auth, self.auth)
+        self.assertEqual(self.git2sc.auth, self.auth)
 
     def test_has_empty_pages_by_default(self):
         'Required to initialize the dictionary'
 
-        self.assertEqual(self.g.pages, {})
+        self.assertEqual(self.git2sc.pages, {})
 
     def test_has_confluence_url_set(self):
         'Required attribute for some methods'
 
-        self.assertEqual(self.g.api_url, self.api_url)
+        self.assertEqual(self.git2sc.api_url, self.api_url)
 
     def test_can_get_page_info(self):
         '''Required to ensure that the get_page_info method calls the correct
         api endpoint and returns a json object'''
 
         page_id = '372274410'
-        result = self.g.get_page_info(page_id)
+        result = self.git2sc.get_page_info(page_id)
         self.assertEqual(
             self.requests.get.assert_called_with(
                 '{}/content/{}?expand=ancestors,body.storage,version'.format(
@@ -67,7 +67,7 @@ class TestGit2SC(unittest.TestCase):
         self.requests.get.return_value.json.return_value = {
             '_expandable': {'homepage': '/rest/api/content/372334010'},
         }
-        result = self.g.get_space_homepage(space_id)
+        result = self.git2sc.get_space_homepage(space_id)
         self.assertEqual(
             self.requests.get.assert_called_with(
                 '{}/space/{}'.format(
@@ -113,7 +113,7 @@ class TestGit2SC(unittest.TestCase):
                 "status": "current",
             },
         }
-        self.g.get_space_articles(space_id)
+        self.git2sc.get_space_articles(space_id)
         self.assertEqual(
             self.requests.get.assert_called_with(
                 '{}/content/?spaceKey={}?expand='
@@ -126,7 +126,7 @@ class TestGit2SC(unittest.TestCase):
             None,
         )
         self.assertTrue(self.requests_error.called)
-        self.assertEqual(self.g.pages, desired_pages)
+        self.assertEqual(self.git2sc.pages, desired_pages)
 
     def test_can_update_articles(self):
         '''Required to ensure that the update_page method posts to the
@@ -134,8 +134,8 @@ class TestGit2SC(unittest.TestCase):
 
         page_id = '372274410'
         html = '<p> This is a test </p>'
-        self.g.pages = {}
-        self.g.pages[page_id] = {
+        self.git2sc.pages = {}
+        self.git2sc.pages[page_id] = {
             'version': {
                 'number': 1
             },
@@ -149,7 +149,7 @@ class TestGit2SC(unittest.TestCase):
                 }
             ]
         }
-        self.g.update_page(page_id, html)
+        self.git2sc.update_page(page_id, html)
 
         data_json = json.dumps({
             'id': page_id,
@@ -188,8 +188,8 @@ class TestGit2SC(unittest.TestCase):
 
         page_id = '372274410'
         html = '<p> This is a test </p>'
-        self.g.pages = {}
-        self.g.update_page(page_id, html)
+        self.git2sc.pages = {}
+        self.git2sc.update_page(page_id, html)
         self.assertEqual(getPageInfoMock.assert_called_with(page_id), None)
 
     def test_can_update_articles_with_title(self):
@@ -198,8 +198,8 @@ class TestGit2SC(unittest.TestCase):
 
         page_id = '372274410'
         html = '<p> This is a test </p>'
-        self.g.pages = {}
-        self.g.pages[page_id] = {
+        self.git2sc.pages = {}
+        self.git2sc.pages[page_id] = {
             'version': {
                 'number': 1
             },
@@ -213,8 +213,8 @@ class TestGit2SC(unittest.TestCase):
                 }
             ]
         }
-        self.g.update_page(page_id, html, 'new title')
-        self.assertEqual(self.g.pages[page_id]['title'], 'new title')
+        self.git2sc.update_page(page_id, html, 'new title')
+        self.assertEqual(self.git2sc.pages[page_id]['title'], 'new title')
 
     def test_can_create_articles_as_parent(self):
         '''Required to ensure that the create_page method posts to the
@@ -222,7 +222,7 @@ class TestGit2SC(unittest.TestCase):
         inheritance is set'''
 
         html = '<p> This is a new page </p>'
-        self.g.create_page('TST', 'new title', html)
+        self.git2sc.create_page('TST', 'new title', html)
 
         data_json = json.dumps({
             'type': 'page',
@@ -254,7 +254,7 @@ class TestGit2SC(unittest.TestCase):
 
         html = '<p> This is a new page </p>'
         parent_id = '372274410'
-        self.g.create_page('TST', 'new title', html, parent_id)
+        self.git2sc.create_page('TST', 'new title', html, parent_id)
 
         data_json = json.dumps({
             'type': 'page',
@@ -289,7 +289,7 @@ class TestGit2SC_requests_error(unittest.TestCase):
         self.api_url = 'https://confluence.sucks.com/wiki/rest/api'
         self.auth_string = 'user:password'
         self.auth = tuple(self.auth_string.split(':'))
-        self.g = Git2SC(self.api_url, self.auth_string)
+        self.git2sc = Git2SC(self.api_url, self.auth_string)
 
         self.requests_object = Mock()
 
@@ -308,7 +308,7 @@ class TestGit2SC_requests_error(unittest.TestCase):
             'statusCode': 400,
             'message': 'Error message',
         })
-        self.g._requests_error(self.requests_object)
+        self.git2sc._requests_error(self.requests_object)
         self.assertEqual(
             self.print.assert_called_with(
                 'Error 400: Error message'
@@ -324,5 +324,5 @@ class TestGit2SC_requests_error(unittest.TestCase):
             'statusCode': 200,
             'message': 'Error message',
         })
-        self.g._requests_error(self.requests_object)
+        self.git2sc._requests_error(self.requests_object)
         self.assertFalse(self.print.called)
